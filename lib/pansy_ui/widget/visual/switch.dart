c8c27d3690b9606ca9ac1982e1e054ebe3d2aea0
Semyon Butenko <laim0n@yandex.ru>
2020-02-26 19:06:00 +0300

    Updated USwitchVisual

--- lib/pansy_ui/widget/visual/switch.dart
+++ lib/pansy_ui/widget/visual/switch.dart
@@ -10,18 +10,38 @@ class USwitchVisual extends StatefulWidget {
 }
 
 class _USwitchVisualState extends State<USwitchVisual> {
+  Duration _duration = Duration(milliseconds: 150);
+
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
-      duration: Duration(milliseconds: 100),
-      width: 30,
-      height: 12,
+      duration: _duration,
+      curve: Curves.ease,
+      width: 40,
+      height: 20,
+      padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+      alignment: widget.value ? Alignment.centerRight : Alignment.centerLeft,
       decoration: BoxDecoration(
-        border: Border.all(color: Theme.of(context).primaryColor, width: 2),
-        color: (widget.value
+        color: widget.value
             ? Theme.of(context).primaryColor
-            : Colors.transparent),
-        borderRadius: BorderRadius.circular(10),
+            : Theme.of(context).primaryColor.withOpacity(0.5),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: AnimatedContainer(
+        duration: _duration,
+        width: 12,
+        height: 12,
+        decoration: BoxDecoration(
+          color: Theme.of(context).scaffoldBackgroundColor,
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              offset: widget.value ? Offset(-1, 1) : Offset(1, 1),
+              blurRadius: 3,
+            ),
+          ],
+        ),
       ),
     );
   }
