4699d7143080d265252ad78e225bec32edaef05c
Semyon Butenko <laim0n@yandex.ru>
2020-02-29 12:48:15 +0300

    Fix name

--- lib/pansy_ui/ui/global/localizations/ui/localizations_bloc_builder.dart
+++ lib/pansy_ui/ui/global/localizations/ui/localizations_bloc_builder.dart
@@ -2,16 +2,16 @@ import 'package:flutter/widgets.dart';
 import '../bloc/bloc.dart';
 import '../localizations_delegates.dart';
 
-/// [LocaleBlocBuilder] является виджетом, содержавший в себе Provider и Builder
+/// [LocalizationsBlocBuilder] является виджетом, содержавший в себе Provider и Builder
 /// BLoC, предназначенного для смены локализации приложения.
-class LocaleBlocBuilder extends StatelessWidget {
+class LocalizationsBlocBuilder extends StatelessWidget {
   final Widget Function(
       BuildContext context,
       Locale locale,
       List<Locale> supportedLocales,
       List<LocalizationsDelegate> localizationsDelegates) builder;
 
-  LocaleBlocBuilder({@required this.builder}) : assert(builder != null);
+  LocalizationsBlocBuilder({@required this.builder}) : assert(builder != null);
 
   @override
   Widget build(BuildContext context) {
