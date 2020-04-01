d2db8701019bb27a0efe7e350d988226026a328c
Semyon Butenko <laim0n@yandex.ru>
2020-04-01 11:15:49 +0300

    Added phone checker

---
+++ lib/pansy_ui/util/device.dart
@@ -0,0 +1,24 @@
+import 'dart:ui' as ui;
+
+class Device {
+  static double devicePixelRatio = ui.window.devicePixelRatio;
+  static ui.Size size = ui.window.physicalSize;
+  static double width = size.width;
+  static double height = size.height;
+  static double screenWidth = width / devicePixelRatio;
+  static double screenHeight = height / devicePixelRatio;
+  static ui.Size screenSize = ui.Size(screenWidth, screenHeight);
+
+  bool get isPhone {
+    bool phone;
+
+    if (devicePixelRatio < 2 && (width >= 1000 || height >= 1000))
+      phone = false;
+    else if (devicePixelRatio == 2 && (width >= 1920 || height >= 1920))
+      phone = false;
+    else
+      phone = true;
+
+    return phone;
+  }
+}
