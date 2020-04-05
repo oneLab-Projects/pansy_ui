9d9eaf67bb6d8bef6fac0927889477503af980cb
Semyon Butenko <laim0n@yandex.ru>
2020-04-05 23:13:27 +0300

    Refactor

--- lib/pansy_ui/ui/global/localizations/bloc/localizations_bloc.dart
+++ lib/pansy_ui/ui/global/localizations/bloc/localizations_bloc.dart
@@ -1,23 +1,17 @@
 import 'package:flutter/widgets.dart';
 import 'package:pansy_ui/pansy_ui/util/localizations.dart';
-import 'package:rxdart/rxdart.dart';
 
 /// Создаёт BLoC (RxDart) для хранения поддерживаемых локалей и рекомендуемой локали.
 class LocalizationsBloc {
   Map<Locale, String> supportedLocales;
   Locale recommendedLocale;
-  BehaviorSubject<bool> _statusStream = BehaviorSubject<bool>.seeded(false);
 
-  LocalizationsBloc(Map<Locale, String> languages) {
-    supportedLocales = languages;
-    LocalizationTools.recommendedLocale(supportedLocales.keys.toList())
-        .then((value) {
-      recommendedLocale = value;
-      _statusStream.sink.add(true);
+  LocalizationsBloc(BuildContext context, String path) {
+    LocalizationTools.getSupportedLocales(context, path).then((value) {
+      supportedLocales = value;
+      LocalizationTools.recommendedLocale(supportedLocales.keys.toList()).then(
+        (value) => recommendedLocale = value,
+      );
     });
   }
-
-  Stream<bool> get isLoaded => _statusStream.stream;
-
-  void dispose() => _statusStream.close();
 }
