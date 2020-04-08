68e2a7ec212a1f3e81118050c4a368a4089324c6
Semyon Butenko <laim0n@yandex.ru>
2020-04-08 21:37:14 +0300

    Fix for web

--- lib/pansy_ui/util/localizations.dart
+++ lib/pansy_ui/util/localizations.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:devicelocale/devicelocale.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'extensions.dart';
@@ -40,8 +41,12 @@ class LocalizationTools {
   /// Возвращает наиболее подходящий язык для пользователя,
   /// основываясь на локализации устройства.
   static Future<Locale> recommendedLocale(List<Locale> supportedLocales) async {
-    String localeLanguage =
-        (await Devicelocale.currentLocale).toLocale().languageCode;
+    String localeLanguage;
+    if (kIsWeb)
+      localeLanguage = 'en';
+    else
+      localeLanguage =
+          (await Devicelocale.currentLocale).toLocale().languageCode;
 
     return supportedLocales.contains(Locale(localeLanguage))
         ? Locale(localeLanguage)
